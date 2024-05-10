6fa831a35976da5ea7ff6f008623e1fdcb09eaa0
Vanshev <pav-alva@ya.ru>
2024-05-10 10:59:17 +0300

    Добавьте файлы проекта.

---
+++ ModManager_Diploma/ViewModel/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModManager_Diploma.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ ModManager_Diploma/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModManager_Diploma.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string _modList;
+
+        public string ModList
+        {
+            get => _modList;
+            set
+            {
+                _modList = value;
+                OnPropertyChanged(nameof(ModList));
+            }
+        }
+
+    }
+}
